e63e968f23639308ea61437596fb4cf60e4e3fa3
sunysty <seokjiseon@gmail.com>
2022-03-07 16:06:45 +0900

    📦  package : 패키지 추가설치

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "@fortawesome/fontawesome-free/js/all.js";
+import { createGlobalStyle } from "styled-components";
+import { RecoilRoot } from "recoil";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+body {
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  color: black;
+  line-height: 1.2;
+  background-color: white;
+}
+a {
+  text-decoration:none;
+  color:inherit;
+}
+`;
+
+ReactDOM.render(
+  <RecoilRoot>
+    <GlobalStyle />
+    <App />
+  </RecoilRoot>,
+  document.getElementById("root")
+);
